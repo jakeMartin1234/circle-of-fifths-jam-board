6645ec6f3e0f1c3c6c9ec8b48530face8dc36b66
Jake Martin <jakemartin@berkeley.edu>
2023-08-06 21:02:42 -0700

    added mobile blocker

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import {Container, Grid, Paper, Typography} from "@mui/material";
+import {Container, Grid, Typography, Link, IconButton} from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const App = () => {
 
@@ -26,9 +27,25 @@ const App = () => {
     <Container>
         <Grid container spacing={5}>
             <Grid item xs={12}>
-                <Typography color="primary" fontSize="30px">
-                    Circle of Fifths Jam Board
-                </Typography>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'left',
+                }}>
+                    <Typography color="primary" fontSize="30px">
+                        Circle of Fifths Jam Board
+                    </Typography>
+                    <Link sx={{ zIndex: 1000,
+                                marginLeft: '30px',
+                                marginTop: '10px',
+                             }}
+                          href='https://github.com/jakeMartin1234/circle-of-fifths-jam-board'
+                          target="_blank"
+                          rel="noopener noreferrer"
+                    >
+                        <GitHubIcon sx={{ color: 'white' }} />
+                    </Link>
+                </div>
+
             </Grid>
             <Grid item xs={12} sm={12} md={4} styl={{ height: '100%', padding: '16px', width: '300px' }}>
                 <MusicPlayer setCurrentNoteIndex={setCurrentNoteIndex}/>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {ThemeProvider} from "@mui/material";
+import {ThemeProvider, Typography} from "@mui/material";
 import theme from './utils/Theme';
+import MediaQuery from 'react-responsive';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ThemeProvider theme={theme}>
+        <MediaQuery minDeviceWidth={1224}>
+            <App />
+        </MediaQuery>
+        <MediaQuery maxDeviceWidth={1224}>
+            <Typography>
+                This app is not optimized for mobile. Please view on a desktop.
+            </Typography>
+        </MediaQuery>
         <App />
     </ThemeProvider>,
 );
